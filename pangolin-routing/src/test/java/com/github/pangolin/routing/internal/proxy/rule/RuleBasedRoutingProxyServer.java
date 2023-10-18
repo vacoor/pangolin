1acb77d1558992f00c30f81540a325be1276c882
vacoor <vacoor@qq.com>
2023-10-18 23:59:52 +0800

    ...

--- pangolin-routing/src/test/java/com/github/pangolin/routing/internal/proxy/rule/RuleBasedRoutingProxyServer.java
+++ pangolin-routing/src/test/java/com/github/pangolin/routing/internal/proxy/rule/RuleBasedRoutingProxyServer.java
@@ -1,25 +1,27 @@
 package com.github.pangolin.routing.internal.proxy.rule;
 
 import com.github.pangolin.routing.internal.proxy.ProxyServer2;
+import com.github.pangolin.routing.internal.proxy.ProxyServerProvider;
 import com.github.pangolin.routing.internal.proxy.ProxyServerStats;
-import com.github.pangolin.routing.internal.proxy.health.HealthCheckScheduler;
 import com.github.pangolin.routing.pattern.DestinationPattern;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import io.netty.channel.ChannelHandler;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.net.InetSocketAddress;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
-public abstract class RuleBasedRoutingProxyServer implements ProxyServer2 {
+public class RuleBasedRoutingProxyServer implements ProxyServer2 {
     private final String name;
-    private final HealthCheckScheduler healthCheckScheduler;
 
-    private final ConcurrentMap<String, Routing> registeredRoutingMap = new ConcurrentHashMap<>();
+    private final ProxyServerProvider proxyServerProvider;
+    private final ConcurrentMap<DestinationPattern, String> registeredRoutingMap = new ConcurrentHashMap<>();
+    /*
     private final LoadingCache<String, ProxyServerStats> statsCache = CacheBuilder.newBuilder()
             .expireAfterAccess(10, TimeUnit.MINUTES)
             .build(new CacheLoader<String, ProxyServerStats>() {
@@ -28,10 +30,11 @@ public abstract class RuleBasedRoutingProxyServer implements ProxyServer2 {
                     return new ProxyServerStats(name);
                 }
             });
+            */
 
-    public RuleBasedRoutingProxyServer(final String name, final HealthCheckScheduler healthCheckScheduler) {
+    public RuleBasedRoutingProxyServer(final String name, final ProxyServerProvider provider) {
         this.name = name;
-        this.healthCheckScheduler = healthCheckScheduler;
+        this.proxyServerProvider = provider;
     }
 
     @Override
@@ -41,50 +44,26 @@ public abstract class RuleBasedRoutingProxyServer implements ProxyServer2 {
 
     @Override
     public ChannelHandler newProxyHandler(final InetSocketAddress sa) {
-        final Set<String> proxies = new HashSet<>();
-        for (final Routing rule : registeredRoutingMap.values()) {
-            if (rule.pattern.matches(sa)) {
-                proxies.add(rule.proxy);
+        for (final Map.Entry<DestinationPattern, String> entry : registeredRoutingMap.entrySet()) {
+            if (!entry.getKey().matches(sa)) {
+                continue;
             }
+            final ProxyServer2 proxyToUse = getProxy(entry.getValue());
+            return null != proxyToUse ? proxyToUse.newProxyHandler(sa) : null;
         }
-        final ProxyServer2 proxyToUse = !proxies.isEmpty() ? choose(sa, proxies) : null;
-        return null != proxyToUse ? proxyToUse.newProxyHandler(sa) : null;
+        return null;
     }
 
-    protected ProxyServer2 choose(final InetSocketAddress sa, final Set<String> proxies) {
-        try {
-            final Map<String, ProxyServerStats> statsMap = statsCache.getAll(proxies);
-            final List<ProxyServerStats> stats = new ArrayList<>(statsMap.values());
-            stats.sort(new Comparator<ProxyServerStats>() {
-                @Override
-                public int compare(final ProxyServerStats o1, final ProxyServerStats o2) {
-                    return Double.compare(o1.getResponseTimeAvg(), o2.getResponseTimeAvg());
-                }
-            });
-            // FIXME
-            return null;
-            // return stats.get(ThreadLocalRandom.current().nextInt(Math.min(upServers.size(), 10)));
-        } catch (ExecutionException e) {
-            return null;
-        }
+    private ProxyServer2 getProxy(final String name) {
+        return proxyServerProvider.getInstance(name);
     }
 
-    public String addRouting(final DestinationPattern pattern, final String proxy) {
-        final String id = UUID.randomUUID().toString();
-        registeredRoutingMap.put(id, new Routing(id, pattern, proxy));
-        return id;
+    public void addRouting(final DestinationPattern pattern, final String proxy) {
+        registeredRoutingMap.put(pattern, proxy);
     }
 
-    public void removeRouting(final String routingId) {
-        registeredRoutingMap.remove(routingId);
-    }
-
-    @Getter
-    @AllArgsConstructor
-    public static class Routing {
-        private final String id;
-        private final DestinationPattern pattern;
-        private final String proxy;
+    public void removeRouting(final DestinationPattern pattern) {
+        registeredRoutingMap.remove(pattern);
     }
 
     public static void main(String[] args) throws ExecutionException {
