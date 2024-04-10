2f0d4e830ab5d36a0c6404011c567bd551d2f918
changhe.yang@YSH8879 <changhe.yang@baozun.com>
2024-04-10 12:35:00 +0800

    。

---
+++ pangolin-routing/src/main/java/com/github/pangolin/routing/config/ProxiesParser.java
@@ -0,0 +1,107 @@
+package com.github.pangolin.routing.config;
+
+import com.github.pangolin.routing.config.clash.ClashProxyServerProviderFactory;
+import com.github.pangolin.routing.config.spi.ServerResolver;
+import com.github.pangolin.routing.proxy.ComposedProxyServerProvider;
+import com.github.pangolin.routing.proxy.ProxyServer;
+import com.github.pangolin.routing.proxy.ProxyServerProvider;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.EventLoopGroup;
+import io.netty.util.internal.ObjectUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+
+/**
+ * TODO DOC ME!.
+ *
+ * @author changhe.yang
+ * @since 20240410
+ */
+public class ProxiesParser {
+
+  public static ProxyServerProvider parse(final InputStream conf, final EventLoopGroup group) throws IOException {
+    return resolve(new InputStreamReader(conf, StandardCharsets.UTF_8), group);
+  }
+
+  public static ComposedProxyServerProvider resolve(final Reader reader, final EventLoopGroup group) throws IOException {
+    ObjectUtil.checkNotNull(reader, "reader");
+    final Map<String, ProxyServer> fixedServers = new LinkedHashMap<>();
+    final List<ProxyServerProvider> providers = new LinkedList<>();
+
+    final BufferedReader r = reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader);
+    String line;
+    while (null != (line = r.readLine())) {
+      final String lineToUse = line.startsWith("#") ? "" : line.trim();
+      if (lineToUse.isEmpty()) {
+        continue;
+      }
+
+      final int i = lineToUse.indexOf('=');
+      if (-1 < i) {
+        final String name = lineToUse.substring(0, i).trim();
+        final String url = lineToUse.substring(i + 1).trim();
+        if (url.startsWith("subscribe:")) {
+          final String subscribeUrl = url.substring("subscribe:".length());
+          ProxyServerProvider proxyServerProvider = ClashProxyServerProviderFactory.create(subscribeUrl).getProxyServerProvider(group);
+          providers.add(proxyServerProvider);
+        } else {
+          final ProxyServer proxyServer = resolve(url);
+          final ProxyServer proxyServerToUse = new ProxyServer() {
+            @Override
+            public String getName() {
+              return name;
+            }
+
+            @Override
+            public ChannelHandler newProxyHandler(final InetSocketAddress sa) {
+              return proxyServer.newProxyHandler(sa);
+            }
+          };
+          fixedServers.put(proxyServer.getName(), proxyServerToUse);
+        }
+      } else {
+        // log
+      }
+    }
+    final ProxyServerProvider[] providersToUse = providers.toArray(new ProxyServerProvider[providers.size() + 1]);
+    providersToUse[providers.size()] = new ProxyServerProvider() {
+      @Override
+      public Collection<ProxyServer> getInstances() {
+        return fixedServers.values();
+      }
+
+      @Override
+      public ProxyServer getInstance(final String name) {
+        return fixedServers.get(name);
+      }
+    };
+    return new ComposedProxyServerProvider(providersToUse);
+  }
+
+  private static ProxyServer resolve(final String url) {
+    final ServiceLoader<ServerResolver> resolvers = ServiceLoader.load(ServerResolver.class);
+    for (final ServerResolver resolver : resolvers) {
+      if (!resolver.acceptsUrl(url)) {
+        continue;
+      }
+      final ProxyServer resolved = resolver.resolve(url, null);
+      if (null != resolved) {
+        return resolved;
+      }
+    }
+    throw new IllegalStateException("NOT found provider: url :" + url);
+  }
+
+}
---
+++ pangolin-routing/src/main/java/com/github/pangolin/routing/config/RulesParser.java
@@ -0,0 +1,31 @@
+package com.github.pangolin.routing.config;
+
+import com.github.pangolin.routing.config.clash.ClashRuleFactory;
+import com.github.pangolin.routing.config.clash.ClashRuleResolver;
+import com.github.pangolin.routing.pattern.DestinationPattern;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * TODO DOC ME!.
+ *
+ * @author changhe.yang
+ * @since 20240410
+ */
+public class RulesParser {
+
+  public static Map<DestinationPattern, String> parseRules(final URL url) throws IOException {
+    return ClashRuleFactory.parseRules(
+        url,
+        ClashRuleResolver.DOMAIN,
+        ClashRuleResolver.DOMAIN_SUFFIX,
+        ClashRuleResolver.DOMAIN_KEYWORD,
+        ClashRuleResolver.IP_CIDR,
+        ClashRuleResolver.IP_CIDR_6,
+        ClashRuleResolver.RULE_SET
+    );
+  }
+
+}
