38f9cd42755d4fec701f4e22558e3d9b92dc9c94
changhe.yang@YSH8879 <changhe.yang@baozun.com>
2024-07-25 13:49:22 +0800

    clean code

---
+++ pangolin-routing/src/main/java/com/github/pangolin/routing/ApplicationLauncher.java
@@ -0,0 +1,17 @@
+package com.github.pangolin.routing;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ApplicationLauncher {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApplicationLauncher.class, args);
+        ApplicationLauncher.main(args);
+    }
+
+}
+
