78586553fbd9ce5b81f16d6efd18d9235d9316a6
changhe.yang@YSH8879 <changhe.yang@baozun.com>
2024-08-16 19:04:29 +0800

    temp fix SS assert

--- pangolin-routing/src/test/java/com/github/pangolin/routing/TunTest.java
+++ pangolin-routing/src/test/java/com/github/pangolin/routing/TunTest.java
@@ -20,6 +20,7 @@ import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
@@ -36,6 +37,46 @@ public class TunTest {
                 : IpV4Packet.newPacket(bytes, 0, bytes.length);
     }
 
+    private static final ThreadLocal<Integer> sequence = new ThreadLocal<Integer>() {
+        @Override
+        protected Integer initialValue() {
+            return 0;
+        }
+    };
+
+    private static TcpPacket.Builder ack(final TcpPacket.TcpHeader header, final InetAddress srcAddr, final InetAddress dstAddr) {
+        final int seq = header.getAcknowledgmentNumber() != 0 ? header.getAcknowledgmentNumber() : 1;
+        return new TcpPacket.Builder()
+                .srcAddr(dstAddr)
+                .dstAddr(srcAddr)
+                .srcPort(header.getDstPort())
+                .dstPort(header.getSrcPort())
+                .options(Arrays.asList()) // FIXME
+                .options(header.getOptions())
+                .sequenceNumber(seq)
+                .acknowledgmentNumber(header.getSequenceNumber() + 1)
+//                .ack(true)
+//                .syn(true)
+                .paddingAtBuild(true)
+                .correctLengthAtBuild(true)
+                .correctChecksumAtBuild(true);
+    }
+
+    private static IpPacket.Builder ack(final IpPacket.Header ipHeader) {
+        return new IpV4Packet.Builder()
+                .version(IpVersion.IPV4)
+                .tos(((IpV4Packet.IpV4Header) ipHeader).getTos())
+                .ttl(((IpV4Packet.IpV4Header) ipHeader).getTtl())
+                .identification(((IpV4Packet.IpV4Header) ipHeader).getIdentification())
+                .fragmentOffset(((IpV4Packet.IpV4Header) ipHeader).getFragmentOffset())
+                .srcAddr(((IpV4Packet.IpV4Header) ipHeader).getDstAddr())
+                .dstAddr(((IpV4Packet.IpV4Header) ipHeader).getSrcAddr())
+                .protocol(IpNumber.TCP)
+                .paddingAtBuild(true)
+                .correctLengthAtBuild(true)
+                .correctChecksumAtBuild(true);
+    }
+
     public static void channelRead0(final ChannelHandlerContext ctx, final IpPacket ipPacket) {
         final IpPacket.IpHeader ipHeader = ipPacket.getHeader();
         final InetAddress srcAddr = ipHeader.getSrcAddr();
@@ -48,41 +89,46 @@ public class TunTest {
             final int acknowledgmentNumber = tcpHeader.getAcknowledgmentNumber();
             final TcpPort tcpSrcPort = tcpHeader.getSrcPort();
             final TcpPort tcpDstPort = tcpHeader.getDstPort();
-            // ONLY SYN
-            if (tcpHeader.getSyn()) {
-                log.info("[SYN] {}:{} -> {}:{}", srcAddr, tcpSrcPort, dstAddr, tcpDstPort);
-//                tcpHeader.getAcknowledgmentNumber()
-                TcpPacket.Builder build = new TcpPacket.Builder()
-                        .srcAddr(dstAddr)
-                        .dstAddr(srcAddr)
-                        .srcPort(tcpDstPort)
-                        .dstPort(tcpSrcPort)
-                        .options(Arrays.asList()) // FIXME
-                        .acknowledgmentNumber(acknowledgmentNumber)
-                        .ack(true)
-                        .syn(true)
-                        .paddingAtBuild(true)
-                        .correctLengthAtBuild(true)
-                        .correctChecksumAtBuild(true);
+
+            log(tcpHeader, ipHeader, true);
+
+            if (!tcpHeader.getUrg() && !tcpHeader.getAck()
+                    && !tcpHeader.getPsh() && !tcpHeader.getRst()
+                    && tcpHeader.getSyn() && !tcpHeader.getFin()) {
+                // C -- [SYN] --> S (handshake)
+                // C <-- [SYN & ACK] -- S (handshake)
+                TcpPacket.Builder outTcpPayload = ack(tcpHeader, srcAddr, dstAddr).ack(true).syn(true);
                 if (ipPacket instanceof IpV4Packet) {
-                    IpV4Packet.Builder builders = new IpV4Packet.Builder()
-                            .version(IpVersion.IPV4)
-                            .tos(((IpV4Packet.IpV4Header) ipHeader).getTos())
-                            .srcAddr((Inet4Address) dstAddr)
-                            .dstAddr((Inet4Address) srcAddr)
-                            .protocol(IpNumber.TCP)
-                            .payloadBuilder(build)
-                            .paddingAtBuild(true)
-                            .correctLengthAtBuild(true)
-                            .correctChecksumAtBuild(true);
-                    IpV4Packet build1 = builders.build();
+                    IpV4Packet out = (IpV4Packet) ack(ipHeader).payloadBuilder(outTcpPayload).build();
+                    log(((TcpPacket)out.getPayload()).getHeader(), out.getHeader(), false);
+                    ctx.writeAndFlush(new Tun4Packet(Unpooled.wrappedBuffer(out.getRawData())));
+                }
+            } else if (!tcpHeader.getUrg() && tcpHeader.getAck()
+                    && !tcpHeader.getPsh() && !tcpHeader.getRst()
+                    && !tcpHeader.getSyn() && !tcpHeader.getFin()) {
+                // ONLY ACK
+                // C -- [ACK] --> S (handshake & disconnect)
+//                log.info("[ACK] {}:{} -> {}:{}", srcAddr, tcpSrcPort, dstAddr, tcpDstPort);
+                Packet payload = tcpPacket.getPayload();
+                System.out.println(payload);
+
+            } else if (!tcpHeader.getUrg() && tcpHeader.getAck()
+                    && !tcpHeader.getPsh() && !tcpHeader.getRst()
+                    && !tcpHeader.getSyn() && tcpHeader.getFin()) {
+                // C -- [ACK & FIN] --> S (disconnect)
+//                log.info("[ACK & FIN] {}:{} -> {}:{}", srcAddr, tcpSrcPort, dstAddr, tcpDstPort);
+
+                // C <-- [ACK] -- S (disconnect)
+                TcpPacket.Builder outTcpPayload = ack(tcpHeader, srcAddr, dstAddr).ack(true);
+                if (ipPacket instanceof IpV4Packet) {
+                    IpV4Packet build1 = (IpV4Packet) ack(ipHeader).payloadBuilder(outTcpPayload).build();
+
+                    IpV4Packet out = (IpV4Packet) ack(ipHeader).payloadBuilder(outTcpPayload).build();
+                    log(((TcpPacket)out.getPayload()).getHeader(), out.getHeader(), false);
                     ctx.writeAndFlush(new Tun4Packet(Unpooled.wrappedBuffer(build1.getRawData())));
                 }
-
-            } else if (tcpHeader.getFin()){
-                log.info("[FIN] {}:{} -> {}:{}", srcAddr, tcpSrcPort, dstAddr, tcpDstPort);
             } else {
-                System.out.println("TCP: " + srcAddr + " -> " + dstAddr);
+//                System.out.println(f + " TCP: " + srcAddr + " -> " + dstAddr);
             }
         } else if (IpNumber.UDP.equals(protocol)) {
             final UdpPacket udpPacket = (UdpPacket) ipPacket.getPayload();
@@ -92,6 +138,33 @@ public class TunTest {
 //            System.out.println("ICMPv6: " + srcAddr + " -> " + dstAddr);
         } else {
 //            System.out.println(protocol.valueAsString());
+        }
+    }
+
+    private static void log(final TcpPacket.TcpHeader tcpHeader, final IpPacket.IpHeader ipHeader, boolean inbound) {
+        String type = "";
+        if (tcpHeader.getUrg()) {
+            type += "[URG]";
+        }
+        if (tcpHeader.getAck()) {
+            type += "[ACK]";
+        }
+        if (tcpHeader.getPsh()) {
+            type += "[PSH]";
+        }
+        if (tcpHeader.getRst()) {
+            type += "[RST]";
+        }
+        if (tcpHeader.getSyn()) {
+            type += "[SYN]";
+        }
+        if (tcpHeader.getFin()) {
+            type += "[FIN]";
+        }
+        if (inbound) {
+            log.info("{}:{} - {} -> {}:{}", ipHeader.getSrcAddr(), tcpHeader.getSrcPort().valueAsInt(), type, ipHeader.getDstAddr(), tcpHeader.getDstPort().valueAsInt());
+        } else {
+            log.info("{}:{} <- {} - {}:{}", ipHeader.getDstAddr(), tcpHeader.getDstPort().valueAsInt(), type, ipHeader.getSrcAddr(), tcpHeader.getSrcPort().valueAsInt());
         }
     }
 
