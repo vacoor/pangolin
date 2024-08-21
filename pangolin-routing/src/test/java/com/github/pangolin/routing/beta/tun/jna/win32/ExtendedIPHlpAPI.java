119089ceb92f2b0397a6aff138194136ef68efe3
changhe.yang@YSH8879 <changhe.yang@baozun.com>
2024-08-21 18:11:43 +0800

    TCP

--- pangolin-routing/src/test/java/com/github/pangolin/routing/beta/tun/jna/win32/ExtendedIPHlpAPI.java
+++ pangolin-routing/src/test/java/com/github/pangolin/routing/beta/tun/jna/win32/ExtendedIPHlpAPI.java
@@ -4,9 +4,19 @@ import com.github.pangolin.routing.beta.tun.jna.win32.iphlp.MibIPInterfaceRow;
 import com.github.pangolin.routing.beta.tun.jna.win32.iphlp.MibUnicastIPAddressRow;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+import com.sun.jna.WString;
+import com.sun.jna.platform.win32.Guid;
 import com.sun.jna.platform.win32.IPHlpAPI;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.PointerByReference;
 import com.sun.jna.win32.W32APIOptions;
+
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public interface ExtendedIPHlpAPI extends IPHlpAPI {
     ExtendedIPHlpAPI INSTANCE = Native.load("IPHlpAPI", ExtendedIPHlpAPI.class, W32APIOptions.DEFAULT_OPTIONS);
@@ -164,4 +174,227 @@ public interface ExtendedIPHlpAPI extends IPHlpAPI {
     int DeleteUnicastIpAddressEntry(MibUnicastIPAddressRow row);
 
     // ------------------------- END UnicastIP related -------------------------
+
+    int GAA_FLAG_SKIP_UNICAST = 0x0001;
+    int GAA_FLAG_SKIP_ANYCAST = 0x0002;
+    int GAA_FLAG_SKIP_MULTICAST = 0x0004;
+    int GAA_FLAG_SKIP_DNS_SERVER = 0x0008;
+    int GAA_FLAG_INCLUDE_PREFIX = 0x0010;
+    int GAA_FLAG_SKIP_FRIENDLY_NAME = 0x0020;
+    int GAA_FLAG_INCLUDE_WINS_INFO = 0x0040;
+    int GAA_FLAG_INCLUDE_GATEWAYS = 0x0080;
+    int GAA_FLAG_INCLUDE_ALL_INTERFACES = 0x0100;
+    int GAA_FLAG_INCLUDE_ALL_COMPARTMENTS = 0x0200;
+    int GAA_FLAG_INCLUDE_TUNNEL_BINDINGORDER = 0x0400;
+
+    int GetAdaptersAddresses(int family, int flags, Pointer reserved, Pointer adapterAddresses, IntByReference sizeOfPointer);
+
+    @Structure.FieldOrder({
+            "Length",
+            "IfIndex",
+            "Next",
+            "AdapterName",
+            "FirstUnicastAddress",
+            "FirstAnycastAddress",
+            "FirstMulticastAddress",
+            "FirstDnsServerAddress",
+            "DnsSuffix",
+            "Description",
+            "FriendlyName",
+            "PhysicalAddress",
+            "PhysicalAddressLength",
+            "Flags",
+            "Mtu",
+            "IfType",
+            "OperStatus",
+            "Ipv6IfIndex",
+            "ZoneIndices",
+            "FirstPrefix",
+            "TransmitLinkSpeed",
+            "ReceiveLinkSpeed",
+            "FirstWinsServerAddress",
+            "FirstGatewayAddress",
+            "Ipv4Metric",
+            "Ipv6Metric",
+            "Luid",
+            "Dhcpv4Server",
+            "CompartmentId",
+            "NetworkGuid",
+            "ConnectionType",
+            "TunnelType",
+            "Dhcpv6Server",
+            "Dhcpv6ClientDuid",
+            "Dhcpv6ClientDuidLength",
+            "Dhcpv6Iaid",
+            "FirstDnsSuffix",
+    })
+    class IP_ADAPTER_ADDRESSES_LH extends Structure {
+        public static class ByReference extends IP_ADAPTER_ADDRESSES_LH
+                implements Structure.ByReference {}
+
+        public IP_ADAPTER_ADDRESSES_LH(Pointer p) {
+            super(p);
+            read();
+        }
+
+        public IP_ADAPTER_ADDRESSES_LH() {}
+
+        public int Length;
+        public int IfIndex;
+
+        public IP_ADAPTER_ADDRESSES_LH.ByReference Next;
+        public String AdapterName;
+        public IP_ADAPTER_UNICAST_ADDRESS_LH.ByReference FirstUnicastAddress;
+        public IP_ADAPTER_ANYCAST_ADDRESS_XP.ByReference FirstAnycastAddress;
+        public IP_ADAPTER_MULTICAST_ADDRESS_XP.ByReference FirstMulticastAddress;
+        public IP_ADAPTER_DNS_SERVER_ADDRESS_XP.ByReference FirstDnsServerAddress;
+        public WString DnsSuffix;
+        public WString Description;
+        public WString FriendlyName;
+        public byte[] PhysicalAddress = new byte[8];
+        public int PhysicalAddressLength;
+        public int Flags;
+        public int Mtu;
+        public int IfType;
+        public int OperStatus;
+        public int Ipv6IfIndex;
+        public int[] ZoneIndices = new int[16];
+        public Pointer FirstPrefix;
+        public long TransmitLinkSpeed;
+        public long ReceiveLinkSpeed;
+        public Pointer FirstWinsServerAddress;
+        public Pointer FirstGatewayAddress;
+        public int Ipv4Metric;
+        public int Ipv6Metric;
+        public WinNT.LUID Luid;
+        public SOCKET_ADDRESS Dhcpv4Server;
+        public int CompartmentId;
+        public Guid.GUID NetworkGuid;
+        public int ConnectionType;
+        public int TunnelType;
+        public SOCKET_ADDRESS Dhcpv6Server;
+        public byte[] Dhcpv6ClientDuid = new byte[130];
+        public int Dhcpv6ClientDuidLength;
+        public int Dhcpv6Iaid;
+        public IP_ADAPTER_DNS_SUFFIX.ByReference FirstDnsSuffix;
+    }
+
+    @Structure.FieldOrder({
+            "Length",
+            "IfIndex",
+            "Next",
+            "Address",
+            "PrefixOrigin",
+            "SuffixOrigin",
+            "DadState",
+            "ValidLifetime",
+            "PreferredLifetime",
+            "LeaseLifetime",
+            "OnLinkPrefixLength"
+    })
+    class IP_ADAPTER_UNICAST_ADDRESS_LH extends Structure {
+        public static class ByReference extends IP_ADAPTER_UNICAST_ADDRESS_LH
+                implements Structure.ByReference {}
+
+        public int Length;
+        public int IfIndex;
+
+        public IP_ADAPTER_UNICAST_ADDRESS_LH.ByReference Next;
+        public SOCKET_ADDRESS Address;
+        public int PrefixOrigin;
+        public int SuffixOrigin;
+        public int DadState;
+        public int ValidLifetime;
+        public int PreferredLifetime;
+        public int LeaseLifetime;
+        public byte OnLinkPrefixLength;
+    }
+
+    @Structure.FieldOrder({"Length", "Reserved", "Next", "Address"})
+    class IP_ADAPTER_DNS_SERVER_ADDRESS_XP extends Structure {
+        public static class ByReference extends IP_ADAPTER_DNS_SERVER_ADDRESS_XP
+                implements Structure.ByReference {}
+
+        public int Length;
+        public int Reserved;
+        public IP_ADAPTER_DNS_SERVER_ADDRESS_XP.ByReference Next;
+        public SOCKET_ADDRESS Address;
+    }
+
+    @Structure.FieldOrder({"Length", "Reserved", "Next", "Address"})
+    class IP_ADAPTER_ANYCAST_ADDRESS_XP extends Structure {
+        public static class ByReference extends IP_ADAPTER_ANYCAST_ADDRESS_XP
+                implements Structure.ByReference {}
+
+        public int Length;
+        public int Reserved;
+        public IP_ADAPTER_DNS_SERVER_ADDRESS_XP.ByReference Next;
+        public SOCKET_ADDRESS Address;
+    }
+
+    @Structure.FieldOrder({"Length", "Reserved", "Next", "Address"})
+    class IP_ADAPTER_MULTICAST_ADDRESS_XP extends Structure {
+        public static class ByReference extends IP_ADAPTER_MULTICAST_ADDRESS_XP
+                implements Structure.ByReference {}
+
+        public int Length;
+        public int Reserved;
+        public IP_ADAPTER_DNS_SERVER_ADDRESS_XP.ByReference Next;
+        public SOCKET_ADDRESS Address;
+    }
+
+    @Structure.FieldOrder({"Next", "_String"})
+    class IP_ADAPTER_DNS_SUFFIX extends Structure {
+        public static class ByReference extends IP_ADAPTER_DNS_SUFFIX
+                implements Structure.ByReference {}
+
+        public IP_ADAPTER_DNS_SUFFIX.ByReference Next;
+        public char[] _String = new char[256];
+    }
+
+    @Structure.FieldOrder({"sin_family", "sin_port", "sin_addr", "sin_zero"})
+    class sockaddr_in extends Structure {
+        public sockaddr_in(Pointer p) {
+            super(p);
+            read();
+        }
+
+        public short sin_family;
+        public short sin_port;
+        public byte[] sin_addr = new byte[4];
+        public byte[] sin_zero = new byte[8];
+    }
+
+    @Structure.FieldOrder({"sin6_family", "sin6_port", "sin6_flowinfo", "sin6_addr", "sin6_scope_id"})
+    class sockaddr_in6 extends Structure {
+        public sockaddr_in6(Pointer p) {
+            super(p);
+            read();
+        }
+
+        public short sin6_family;
+        public short sin6_port;
+        public int sin6_flowinfo;
+        public byte[] sin6_addr = new byte[16];
+        public int sin6_scope_id;
+    }
+
+    @Structure.FieldOrder({"lpSockaddr", "iSockaddrLength"})
+    class SOCKET_ADDRESS extends Structure {
+        public Pointer lpSockaddr;
+        public int iSockaddrLength;
+
+        InetAddress toAddress() throws UnknownHostException {
+            switch (lpSockaddr.getShort(0)) {
+                case AF_INET:
+                    sockaddr_in in4 = new sockaddr_in(lpSockaddr);
+                    return InetAddress.getByAddress(in4.sin_addr);
+                case AF_INET6:
+                    sockaddr_in6 in6 = new sockaddr_in6(lpSockaddr);
+                    return Inet6Address.getByAddress("", in6.sin6_addr, in6.sin6_scope_id);
+            }
+
+            return null;
+        }
+    }
 }
--- pangolin-routing/src/test/java/com/github/pangolin/routing/beta/tun/jna/win32/Main.java
+++ pangolin-routing/src/test/java/com/github/pangolin/routing/beta/tun/jna/win32/Main.java
@@ -1,5 +1,10 @@
 package com.github.pangolin.routing.beta.tun.jna.win32;
 
+import static com.github.pangolin.routing.beta.tun.jna.win32.ExtendedIPHlpAPI.GAA_FLAG_SKIP_ANYCAST;
+import static com.github.pangolin.routing.beta.tun.jna.win32.ExtendedIPHlpAPI.GAA_FLAG_SKIP_FRIENDLY_NAME;
+import static com.github.pangolin.routing.beta.tun.jna.win32.ExtendedIPHlpAPI.GAA_FLAG_SKIP_MULTICAST;
+import static com.github.pangolin.routing.beta.tun.jna.win32.ExtendedIPHlpAPI.GAA_FLAG_SKIP_UNICAST;
+import static com.sun.jna.platform.win32.IPHlpAPI.AF_UNSPEC;
 import static org.drasyl.channel.tun.jna.windows.Wintun.WintunCloseAdapter;
 import static org.drasyl.channel.tun.jna.windows.Wintun.WintunCreateAdapter;
 import static org.drasyl.channel.tun.jna.windows.Wintun.WintunEndSession;
@@ -15,10 +20,13 @@ import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.WString;
 import com.sun.jna.platform.win32.IPHlpAPI;
+import com.sun.jna.platform.win32.Win32Exception;
 import com.sun.jna.platform.win32.WinError;
+import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.LongByReference;
 import com.sun.jna.ptr.PointerByReference;
 import freework.codec.Hex;
+import lombok.extern.slf4j.Slf4j;
 import org.drasyl.channel.tun.jna.windows.Guid;
 import org.drasyl.channel.tun.jna.windows.WinDef;
 import org.drasyl.channel.tun.jna.windows.Wintun;
@@ -27,12 +35,18 @@ import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
+@Slf4j
 public class Main {
 
     public static void main(String[] args) throws IOException {
 //        System.out.println(NetioAPI.INSTANCE);
+        listAssociatedAddresses(AF_UNSPEC);
+        getAdapterAddresses();
+
+        System.exit(0);
         final String id = "{22430978-1194-4d70-b652-f1546d123aff}";
         final String s = id.replaceAll("^\\{|\\}$|-", "");
         final Guid.GUID guid = Guid.GUID.fromBinary(Hex.decode(s));
@@ -139,4 +153,63 @@ public class Main {
         ExtendedIPHlpAPI.INSTANCE.FreeMibTable(table.getPointer());
     }
 
+
+    private static void getAdapterAddresses() {
+        // The recommended method of calling the GetAdaptersAddresses function is to pre-allocate a
+        // 15KB working buffer
+        Memory buffer = new Memory(15 * 1024L);
+        IntByReference size = new IntByReference(0);
+        int flags =
+                GAA_FLAG_SKIP_UNICAST
+                        | GAA_FLAG_SKIP_ANYCAST
+                        | GAA_FLAG_SKIP_MULTICAST
+                        | GAA_FLAG_SKIP_FRIENDLY_NAME;
+        int error = ExtendedIPHlpAPI.INSTANCE.GetAdaptersAddresses(AF_UNSPEC, flags, Pointer.NULL, buffer, size);
+        if (error == WinError.ERROR_BUFFER_OVERFLOW) {
+            buffer = new Memory(size.getValue());
+            error = ExtendedIPHlpAPI.INSTANCE.GetAdaptersAddresses(AF_UNSPEC, flags, Pointer.NULL, buffer, size);
+            if (error != WinError.ERROR_SUCCESS) {
+                throw new Win32Exception(error);
+            }
+        }
+
+        ExtendedIPHlpAPI.IP_ADAPTER_ADDRESSES_LH result = new ExtendedIPHlpAPI.IP_ADAPTER_ADDRESSES_LH(buffer);
+        do {
+            // only interfaces with IfOperStatusUp
+            if (result.OperStatus == 1) {
+                ExtendedIPHlpAPI.IP_ADAPTER_DNS_SERVER_ADDRESS_XP dns = result.FirstDnsServerAddress;
+                while (dns != null) {
+                    InetAddress address;
+                    try {
+                        address = dns.Address.toAddress();
+                        System.out.println(address);
+
+                        if (address instanceof Inet4Address || !address.isSiteLocalAddress()) {
+//                            addNameserver(new InetSocketAddress(address, SimpleResolver.DEFAULT_PORT));
+                        } else {
+                            log.debug(
+                                    "Skipped site-local IPv6 server address {} on adapter index {}",
+                                    address,
+                                    result.IfIndex);
+                        }
+                    } catch (UnknownHostException e) {
+                        log.warn("Invalid nameserver address on adapter index {}", result.IfIndex, e);
+                    }
+
+                    dns = dns.Next;
+                }
+
+                log.warn(result.DnsSuffix.toString());
+//                addSearchPath(result.DnsSuffix.toString());
+                ExtendedIPHlpAPI.IP_ADAPTER_DNS_SUFFIX suffix = result.FirstDnsSuffix;
+                while (suffix != null) {
+//                    addSearchPath(String.valueOf(suffix._String));
+                    System.out.println(String.valueOf(suffix._String));
+                    suffix = suffix.Next;
+                }
+            }
+
+            result = result.Next;
+        } while (result != null);
+    }
 }
