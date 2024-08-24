d1a4ad68e33e362b1ff80ef3b8ff2b89b2cf3122
changhe.yang@YSH8879 <changhe.yang@baozun.com>
2024-08-24 21:56:22 +0800

    ...

--- pangolin-routing/src/test/java/com/github/pangolin/routing/tun/wintun/win32/Main.java
+++ pangolin-routing/src/test/java/com/github/pangolin/routing/tun/wintun/win32/Main.java
@@ -1,6 +1,6 @@
 package com.github.pangolin.routing.tun.wintun.win32;
 
-import static com.github.pangolin.routing.tun.wintun.win32.IpHelpLib.*;
+import static com.github.pangolin.routing.tun.wintun.win32.IpHelpLib.AF_INET;
 import static org.drasyl.channel.tun.jna.windows.Wintun.WintunCloseAdapter;
 import static org.drasyl.channel.tun.jna.windows.Wintun.WintunCreateAdapter;
 import static org.drasyl.channel.tun.jna.windows.Wintun.WintunEndSession;
@@ -19,13 +19,12 @@ import org.drasyl.channel.tun.jna.windows.Wintun;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
 
 @Slf4j
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        /*
         final int family = AF_INET;
         // final long interfaceLuid = NetworkInterfaceEx.interfaceAliasToLuid("以太网 2");
         final Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
@@ -39,15 +38,14 @@ public class Main {
             System.out.println(String.format("[%s] %s / %s -> %s", index, name, alias, mtu));
 
             final long luid = NetworkInterfaceEx.interfaceIndexToLuid(index);
-            int mtu1 = NetworkInterfaceEx.getMTU(luid, AF_INET);
-            System.out.println(mtu1);
+//            int mtu1 = NetworkInterfaceEx.getMTU(luid, AF_INET);
+//            System.out.println(mtu1);
 //            ni.getInetAddresses()
 //            ni.getInterfaceAddresses().get(0).getNetworkPrefixLength()
         }
-
-
         System.out.println();
         System.exit(0);
+        */
         final Guid.GUID guid = Guid.GUID.newGuid();
 
 //        listAssociatedAddresses(IPHlpAPI.AF_UNSPEC);
@@ -63,23 +61,6 @@ public class Main {
             NetworkInterfaceEx.setInterfaceDns(NetworkInterfaceEx.interfaceLuidToGuid(luid), AF_INET, new InetAddress[]{InetAddress.getByName("198.18.0.2")}, new String[0]);
 
             session = WintunStartSession(adapter, new WinDef.DWORD(0x400000));
-
-            final MIB_IPINTERFACE_ROW row = new MIB_IPINTERFACE_ROW();
-
-            INSTANCE.InitializeIpInterfaceEntry(row);
-            row.Family = AF_INET;
-            row.InterfaceLuid = luid;
-
-            int i = INSTANCE.GetIpInterfaceEntry(row);
-            System.out.println(row.Metric);
-            System.out.println(row.NlMtu);
-            System.out.println();
-//            flushInterfaceAddresses(luid, AF_UNSPEC);
-
-            row.Metric = 6;
-            row.NlMtu = 65500;
-
-            int i1 = INSTANCE.SetIpInterfaceEntry(row);
 
             System.out.println();
         } catch (final LastErrorException e) {
