db64eb4522235d5323a1215e3b51b131fe2aba1d
changhe.yang@YSH8879 <changhe.yang@baozun.com>
2025-05-08 18:19:54 +0800

    ...

--- pangolin-routing/src/main/java/com/github/pangolin/routing/upstream/UpstreamCombiner.java
+++ pangolin-routing/src/main/java/com/github/pangolin/routing/upstream/UpstreamCombiner.java
@@ -3,12 +3,13 @@ package com.github.pangolin.routing.upstream;
 import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 
-public abstract class UpstreamCombiner {
+public interface UpstreamCombiner {
 
     public abstract String name();
 
     public abstract Upstream combine(final String name, final Iterable<String> names, final UpstreamRegistry registry);
 
+    /*
     public static UpstreamCombiner getInstance(final String name) {
         for (final UpstreamCombiner combiner : ServiceLoader.load(UpstreamCombiner.class)) {
             if (combiner.name().equals(name)) {
@@ -17,5 +18,6 @@ public abstract class UpstreamCombiner {
         }
         throw new ServiceConfigurationError(name);
     }
+    */
 
 }
