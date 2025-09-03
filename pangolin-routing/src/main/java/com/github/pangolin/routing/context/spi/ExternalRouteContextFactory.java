99c8ec63f8e2f9d8035bf4f1323647c51fa808a2
vacoor <vacoor@qq.com>
2025-09-03 21:56:20 +0800

    TCP 协议优化

--- pangolin-routing/src/main/java/com/github/pangolin/routing/context/spi/ExternalRouteContextFactory.java
+++ pangolin-routing/src/main/java/com/github/pangolin/routing/context/spi/ExternalRouteContextFactory.java
@@ -130,6 +130,7 @@ public class ExternalRouteContextFactory extends AbstractRouteContextFactory {
             Preconditions.checkState(HttpURLConnection.HTTP_OK == responseCode, "responseCode = %s", responseCode);
 
             final String responseBody = Http.getResponseBodyAsString(httpUrlConnection);
+            log.info("Configuration: \n{}", responseBody);
             return load(new ByteArrayInputStream(responseBody.getBytes(StandardCharsets.UTF_8)));
         } finally {
             Http.close(httpUrlConnection);
